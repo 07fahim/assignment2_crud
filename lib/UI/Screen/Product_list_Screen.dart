b552f2c8e0f71fbc0549264312c94013e42ece6b
Fahim Fiyaz <faiyazfahim743@gmail.com>
2024-12-14 13:21:37 +0600

    add

--- lib/UI/Screen/Product_list_Screen.dart
+++ lib/UI/Screen/Product_list_Screen.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:assignment2_crud/Widgets/product_item.dart';
 import 'package:assignment2_crud/models/product.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart';
 
 class ProductListScreen extends StatefulWidget {
   const ProductListScreen({super.key});
@@ -12,6 +15,12 @@ class ProductListScreen extends StatefulWidget {
 
 class _ProductListScreenState extends State<ProductListScreen> {
   List<Product> productList=[];
+
+  @override
+  void initState() {
+    super.initState();
+    _getProductList();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,8 +34,37 @@ class _ProductListScreenState extends State<ProductListScreen> {
       body: ListView.builder(
           itemCount: productList.length,
           itemBuilder: (context,index){
-        return
+        return ProductItem(product: productList[index]);
       }),
     );
   }
+  Future<void> _getProductList()async{
+    Uri uri =Uri.parse("https://crud.teamrabbil.com/api/v1/ReadProduct");
+    Response response =await get(uri);
+    print(response.statusCode);
+    print(response.body);
+    if(response.statusCode==200){
+      final decodedData=jsonDecode(response.body);
+      print(decodedData['status']);
+      for(Map<String,dynamic> p in decodedData['data']){
+        Product product=Product(
+            id: p['_id'],
+            productName: p['ProductName'],
+            productCode: p['ProductCode'],
+            image: p['Img'],
+            unitPrice: p['UnitPrice'],
+            quantity: p['Qty'],
+            totalPrice: p['TotalPrice'],
+            createdDate: p['CreatedDate']
+        );
+        productList.add(product);
+      }
+      setState(() {
+
+      });
+    }
+
+  }
+
 }
+
--- lib/Widgets/product_item.dart
+++ lib/Widgets/product_item.dart
@@ -1,8 +1,6 @@
+import 'package:assignment2_crud/UI/Style/style.dart';
 import 'package:flutter/material.dart';
-
 import '../models/product.dart';
-
-
 
 class ProductItem extends StatelessWidget {
   const ProductItem({super.key, required this.product});
@@ -15,6 +13,7 @@ class ProductItem extends StatelessWidget {
       leading: Image.network(
         product.image ?? '',
         width: 40,
+        errorBuilder: (context, error, stackTrace) => const Icon(Icons.image_not_supported, size: 40),
       ),
       title: Text(product.productName ?? 'Unknown'),
       subtitle: Column(
@@ -26,15 +25,50 @@ class ProductItem extends StatelessWidget {
           Text('Total Price: ${product.totalPrice ?? 'Unknown'}'),
         ],
       ),
-      trailing: Wrap(
+      trailing: Row(
+        mainAxisSize: MainAxisSize.min,
         children: [
-          IconButton(onPressed: () {
-            // TODO: implement delete product api
-          }, icon: const Icon(Icons.delete)),
-          IconButton(
-            onPressed: () {
-            },
-            icon: const Icon(Icons.edit),
+          Container(
+            width: 40,
+            height: 40,
+            decoration:  const BoxDecoration(
+              color: colorGrey,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(8),
+                bottomLeft: Radius.circular(8),
+              ),
+            ),
+            child: IconButton(
+              padding: EdgeInsets.zero,
+              onPressed: () {
+                // TODO: implement delete product api
+              },
+              icon: const Icon(
+                Icons.delete,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Container(
+            width: 40,
+            height: 40,
+            decoration: const BoxDecoration(
+              color: colorBlue,
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(8),
+                bottomRight: Radius.circular(8),
+              ),
+            ),
+            child: IconButton(
+              padding: EdgeInsets.zero,
+              onPressed: () {
+                // TODO: implement edit functionality
+              },
+              icon: const Icon(
+                Icons.edit,
+                color: Colors.white,
+              ),
+            ),
           ),
         ],
       ),
