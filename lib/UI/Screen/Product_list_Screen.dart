1bfde5a83831eceea2bc7214c8d3ab4c089e9d9f
Fahim Fiyaz <faiyazfahim743@gmail.com>
2024-12-14 23:29:53 +0600

    add

--- lib/UI/Screen/Product_list_Screen.dart
+++ lib/UI/Screen/Product_list_Screen.dart
@@ -42,115 +42,123 @@ class _ProductListScreenState extends State<ProductListScreen> {
         centerTitle: true,
         titleTextStyle: GoogleFonts.poppins(
             fontSize: 22, color: Colors.black, fontWeight: FontWeight.w500),
+        actions: [IconButton(onPressed: (){
+          _getProductList();
+        }, icon: const Icon(Icons.refresh_sharp))],
       ),
       body: Stack(
         children: [
           ScreenBackground(context),
-          Visibility(
-            visible: _getProductListInProgress == false,
-            replacement: const Center(
-              child: CircularProgressIndicator(),
-            ),
-            child: ListView.builder(
-              padding: const EdgeInsets.all(16),
-              itemCount: productList.length,
-              itemBuilder: (context, index) {
-                return Card(
-                  margin: const EdgeInsets.only(bottom: 16),
-                  child: Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: Row(
-                      children: [
-                        Image.network(
-                          productList[index].image ?? 'Unknown',
-                          width: 60,
-                          height: 60,
-                          errorBuilder: (context, error, stackTrace) =>
-                              const Icon(Icons.image_not_supported, size: 60),
-                        ),
-                        const SizedBox(width: 16),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
+          RefreshIndicator(
+            onRefresh: () async{
+              _getProductList();
+            },
+            child: Visibility(
+              visible: _getProductListInProgress == false,
+              replacement: const Center(
+                child: CircularProgressIndicator(),
+              ),
+              child: ListView.builder(
+                padding: const EdgeInsets.all(16),
+                itemCount: productList.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    margin: const EdgeInsets.only(bottom: 16),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Row(
+                        children: [
+                          Image.network(
+                            productList[index].image ?? 'Unknown',
+                            width: 60,
+                            height: 60,
+                            errorBuilder: (context, error, stackTrace) =>
+                                const Icon(Icons.image_not_supported, size: 60),
+                          ),
+                          const SizedBox(width: 16),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  productList[index].productName ?? 'Unknown',
+                                  style: GoogleFonts.aBeeZee(
+                                    fontSize: 18,fontWeight: FontWeight.w700
+                                  )
+                                ),
+                                const SizedBox(height: 4),
+                                Text(
+                                  'Product Code: ${productList[index].productCode ?? 'Unknown'}',
+                                  style: const TextStyle(
+                                      color: Colors.grey,
+                                      fontStyle: FontStyle.italic),
+                                ),
+                                Text(
+                                  'Quantity: ${productList[index].quantity ?? 'Unknown'}',
+                                  style: const TextStyle(
+                                      color: Colors.grey,
+                                      fontStyle: FontStyle.italic),
+                                ),
+                                Text(
+                                  'Price: ${productList[index].unitPrice ?? 'Unknown'}',
+                                  style: const TextStyle(
+                                      color: Colors.grey,
+                                      fontStyle: FontStyle.italic),
+                                ),
+                                Text(
+                                  'Total Price: ${productList[index].totalPrice ?? 'Unknown'}',
+                                  style: const TextStyle(
+                                      color: Colors.lime,
+                                      fontStyle: FontStyle.italic),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Row(
+                            mainAxisSize: MainAxisSize.min,
                             children: [
-                              Text(
-                                productList[index].productName ?? 'Unknown',
-                                style: GoogleFonts.aBeeZee(
-                                  fontSize: 18,fontWeight: FontWeight.w700
-                                )
+                              Container(
+                                width: 40,
+                                height: 40,
+                                decoration:  BoxDecoration(
+                                  color: Colors.red.shade500,
+                                  borderRadius: const BorderRadius.only(
+                                    topLeft: Radius.circular(8),
+                                    bottomLeft: Radius.circular(8),
+                                  ),
+                                ),
+                                child: IconButton(
+                                  padding: EdgeInsets.zero,
+                                  onPressed: () {},
+                                  icon: const Icon(Icons.delete,
+                                      color: Colors.white),
+                                ),
                               ),
-                              const SizedBox(height: 4),
-                              Text(
-                                'Product Code: ${productList[index].productCode ?? 'Unknown'}',
-                                style: const TextStyle(
-                                    color: Colors.grey,
-                                    fontStyle: FontStyle.italic),
-                              ),
-                              Text(
-                                'Quantity: ${productList[index].quantity ?? 'Unknown'}',
-                                style: const TextStyle(
-                                    color: Colors.grey,
-                                    fontStyle: FontStyle.italic),
-                              ),
-                              Text(
-                                'Price: ${productList[index].unitPrice ?? 'Unknown'}',
-                                style: const TextStyle(
-                                    color: Colors.grey,
-                                    fontStyle: FontStyle.italic),
-                              ),
-                              Text(
-                                'Total Price: ${productList[index].totalPrice ?? 'Unknown'}',
-                                style: const TextStyle(
-                                    color: Colors.lime,
-                                    fontStyle: FontStyle.italic),
+                              Container(
+                                width: 40,
+                                height: 40,
+                                decoration: const BoxDecoration(
+                                  color:Colors.lightBlueAccent,
+                                  borderRadius: BorderRadius.only(
+                                    topRight: Radius.circular(8),
+                                    bottomRight: Radius.circular(8),
+                                  ),
+                                ),
+                                child: IconButton(
+                                  padding: EdgeInsets.zero,
+                                  onPressed: () {},
+                                  icon:
+                                      const Icon(Icons.edit, color: Colors.white),
+                                ),
                               ),
                             ],
                           ),
-                        ),
-                        Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Container(
-                              width: 40,
-                              height: 40,
-                              decoration: const BoxDecoration(
-                                color: colorGrey,
-                                borderRadius: BorderRadius.only(
-                                  topLeft: Radius.circular(8),
-                                  bottomLeft: Radius.circular(8),
-                                ),
-                              ),
-                              child: IconButton(
-                                padding: EdgeInsets.zero,
-                                onPressed: () {},
-                                icon: const Icon(Icons.delete,
-                                    color: Colors.white),
-                              ),
-                            ),
-                            Container(
-                              width: 40,
-                              height: 40,
-                              decoration: const BoxDecoration(
-                                color: colorBlue,
-                                borderRadius: BorderRadius.only(
-                                  topRight: Radius.circular(8),
-                                  bottomRight: Radius.circular(8),
-                                ),
-                              ),
-                              child: IconButton(
-                                padding: EdgeInsets.zero,
-                                onPressed: () {},
-                                icon:
-                                    const Icon(Icons.edit, color: Colors.white),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                );
-              },
+                  );
+                },
+              ),
             ),
           ),
         ],
@@ -173,6 +181,7 @@ class _ProductListScreenState extends State<ProductListScreen> {
   }
 
   Future<void> _getProductList() async {
+    productList.clear();
     _getProductListInProgress = true;
     setState(() {});
     Uri uri = Uri.parse("https://crud.teamrabbil.com/api/v1/ReadProduct");
