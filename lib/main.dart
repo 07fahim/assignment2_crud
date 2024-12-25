9b26e06a850360fcf00610969c2e04ecff35d539
Fahim Fiyaz <faiyazfahim743@gmail.com>
2024-12-25 23:14:32 +0600

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,9 @@
-import 'package:assignment2_crud/utils/theme_provider.dart';
+
+import 'package:assignment2_crud/app.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 
 void main() {
-  runApp(
-    ChangeNotifierProvider(
-      create: (_) => ThemeProvider(),
-      child: const CrudApp(),
-    ),
-  );
-}
-
-class CrudApp extends StatelessWidget {
-  const CrudApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Consumer<ThemeProvider>(
-      builder: (context, themeProvider, child) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          themeMode: themeProvider.themeMode,
-          theme: themeProvider.lightTheme,
-          darkTheme: themeProvider.darkTheme,
-          // ... rest of your route configuration
-        );
-      },
-    );
-  }
+  runApp(const CrudApp());
 }
 
