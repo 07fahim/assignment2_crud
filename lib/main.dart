42c6cd87078993ac2528cc4cf12ebeb84ea612e4
Fahim Fiyaz <faiyazfahim743@gmail.com>
2024-12-25 21:33:00 +0600

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 
 import 'package:assignment2_crud/app.dart';
+import 'package:assignment2_crud/utils/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const CrudApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => ThemeProvider(),
+      child: const CrudApp(),
+    ),
+  );
 }
 
